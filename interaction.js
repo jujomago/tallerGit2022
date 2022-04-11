225673a16f3076f7ee4a7e4ce7d36d8e0fd5ab22
Josue Mancilla <jujomago@gmail.com>
2022-04-11 13:30:58 -0400

    added functionality to escape from pointer

--- interaction.js
+++ interaction.js
@@ -8,18 +8,33 @@ const playAnimation = (e)=>{
     if(animate.checked){
         shapes.forEach(shape => {     
             const animationSelected= Math.floor(Math.random() * animations.length);
-            shape.classList.add(`animate__${animations[animationSelected]}`)
+            shape.classList.add('animate__animated',`animate__${animations[animationSelected]}`, 'animate__infinite')
+        })
+    }else{
+        console.log('aaaa');
+        shapes.forEach(shape => {                 
+            shape.classList.remove(...animations.map(item=>'animate__'+item))
         })
     }
 }
+
+const escapefromMouse = (e)=>{    
+    if(escaping.checked){
+        e.target.style.top= ( Math.random() * 300)+"px";
+        e.target.style.left= ( Math.random() * 300)+"px";
+    }
+}
+
 
 (()=>{ 
  shapes= document.querySelectorAll('.withtext') 
  title1 = document.querySelector('h1') 
  animate = document.querySelector('#cbox1')
- animate.addEventListener('click',playAnimation)
+ escaping = document.querySelector('#cbox2')
+ animate.addEventListener('click',playAnimation) 
  shapes.forEach(shape => {     
      shape.addEventListener('click',showMessage)
+     shape.addEventListener('mouseenter',escapefromMouse);
  })
 })()
 
