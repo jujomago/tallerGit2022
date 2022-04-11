cdf30b0dde84c13b12c1e50b7145f828ba077908
Josue Mancilla <jujomago@gmail.com>
2022-04-11 11:22:32 -0400

    updated styles, added h1 to show student name

--- interaction.js
+++ interaction.js
@@ -1,9 +1,12 @@
-const showMessage = (e) => {
-    alert(e.target.innerText);
+let shapes,title1;
+const showMessage = (e) => {    
+    console.log(title1);
+    title1.innerText=e.target.innerText;
 }
 
 (()=>{ 
- const shapes= document.querySelectorAll('.withtext') 
+ shapes= document.querySelectorAll('.withtext') 
+ title1 = document.querySelector('h1') 
  shapes.forEach(shape => {     
      shape.addEventListener('click',showMessage)
  })
