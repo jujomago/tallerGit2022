ddf93e8a108a7ecf6a813a909fc4152daca4d5d7
Josue Mancilla <jujomago@gmail.com>
2022-04-11 12:05:26 -0400

    added animations toogle

--- interaction.js
+++ interaction.js
@@ -1,12 +1,23 @@
-let shapes,title1;
+let shapes,title1,animate;
 const showMessage = (e) => {    
     console.log(title1);
     title1.innerText=e.target.innerText;
+}
+const playAnimation = (e)=>{
+    const animations = ['bounce','wooble','heartBeat','rubberBand','hinge','jackInTheBox','bounceOut']
+    if(animate.checked){
+        shapes.forEach(shape => {     
+            const animationSelected= Math.floor(Math.random() * animations.length);
+            shape.classList.add(`animate__${animations[animationSelected]}`)
+        })
+    }
 }
 
 (()=>{ 
  shapes= document.querySelectorAll('.withtext') 
  title1 = document.querySelector('h1') 
+ animate = document.querySelector('#cbox1')
+ animate.addEventListener('click',playAnimation)
  shapes.forEach(shape => {     
      shape.addEventListener('click',showMessage)
  })
